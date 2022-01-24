193a21d23c335fb64439032c85580c33ae23706a
jmac <clavedeminimac>
2022-01-24 12:37:00 -0500

    [REF] se renombro la ruta del paquete

---
+++ app/src/main/java/dev/javiersolis/architecture/application/example/ui/main/MainViewModel.kt
@@ -0,0 +1,7 @@
+package dev.javiersolis.architecture.application.example.ui.main
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
